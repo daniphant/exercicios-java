ec1c94f979a01e1793c9b5edf771ea4858089043
daniphant <daniphnt_dev@yahoo.com>
2020-09-22 15:50:22 -0300

    Add Comment

--- Etc/Hello Around the World/HelloWorld.java
+++ Etc/Hello Around the World/HelloWorld.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
+// Prints hello world in various languages by printing each line in hello_unicode.txt
 public class HelloWorld {
 	public static void main(String[] args) throws IOException {
 		try (BufferedReader reader = new BufferedReader(new FileReader("hello_unicode.txt"))) {
