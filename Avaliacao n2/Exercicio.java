a0886b8e20d7a4961de5f699f776cedfef588341
daniphant <daniphant@protonmail.com>
2020-09-22 15:35:09 -0300

    Completed Avaliacao n2

--- Avaliacao n2/Exercicio.java
+++ Avaliacao n2/Exercicio.java
@@ -6,20 +6,21 @@ class Aluno {
     public Aluno() {
         super();
         nome = new String();
+        notas = new double[4];
     }
 
-    float media(){
-        return (notas[0] + notas[1] + notas[2] + notas[3])/4;
+    double media(){
+        return (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
     }
-    float maior(){
-        int maior = -1;
-        for(float nota : notas)
+    double maior(){
+        double maior = -1;
+        for(double nota : notas)
             if (nota > maior) maior = nota;
         return maior;
     }
-    float menor(){
-        int menor = 101;
-        for(float nota : notas) 
+    double menor(){
+        double menor = 101;
+        for(double nota : notas)
             if (nota < menor) menor = nota;
         return menor;
     }
@@ -29,12 +30,10 @@ class Aluno {
     }
 }
 
-public class Exercicio() {
-
-    private static final int SIZE = 3;
+public class Exercicio {
     public static void main(String[] args) {
-        Aluno[] alunos = new Aluno[SIZE];
         Scanner scan = new Scanner(System.in);
+        Aluno[] alunos = new Aluno[3];
 
         for (int i = 0; i < alunos.length; i++){
             alunos[i] = new Aluno();
@@ -44,8 +43,21 @@ public class Exercicio() {
             for(int j = 0; j < 4; j++)
             {
                 System.out.println("Nota " + (j+1) + ":");
-                alunos[i].notas[j] = scan.nextDouble();
+                do {
+                    alunos[i].notas[j] = scan.nextDouble();
+                    if (alunos[i].notas[j] > 100 || alunos[i].notas[j] < 0)
+                        System.out.println("Nota inválida. Seu valor deve ser entre 0 e 100.");
+                } while(alunos[i].notas[j] > 100 || alunos[i].notas[j] < 0);
             }
+            scan.nextLine();
         }
+        for(Aluno a : alunos) {
+            System.out.println(a.nome+"\nSua media é: " + a.media());
+            System.out.println("Sua maior nota é: " + a.maior());
+            System.out.println("Sua menor nota é: " + a.menor());
+            if (a.estaAprovado()) System.out.println("Parabéns! Você foi aprovado!\n");
+            else System.out.println("Você foi reprovado.\n");
+        }
+        scan.close();
     }
 }
